5a6cfdd828a6914c51c0beb15d42a1bbb427e823
JuanGalarza <groupfuzion@gmail.com>
2022-07-11 00:09:08 -0300

    Agregamos Card a index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+import Card from "../components/card";
 
 export default function Index() {
-    return (
+  return (
+    <div>
       <h1>Hola mundo!</h1>
-    );
-  }
+      <Card />
+    </div>
+  );
+}
