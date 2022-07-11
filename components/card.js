51681d55da990d8ef225ae8c00848b285a7234f9
JuanGalarza <groupfuzion@gmail.com>
2022-07-11 00:08:26 -0300

    Modificacion de card.js

--- components/card.js
+++ components/card.js
@@ -2,6 +2,20 @@ import React from 'react';
 
 export default function Index() {
     return (
-      <h1>Hola mundo!</h1>
+      <div className='card'>
+        <div className='card-img'>
+          <img src='./vercel.svg' />
+        </div>
+        <hr />
+        <div className='card-title'>
+          <h1>Vercel</h1>
+        </div>
+        <div className='card-desc'>
+          <p>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</p>
+        </div>
+        <div className='card-btn'>
+          <a href='#'>Button</a>
+        </div>
+      </div>
     );
   }
